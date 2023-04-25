050049a457964a4c6ceffd64b248e75e3e6ea3bc
Gytis Vaicius <gyteniz@gmail.com>
2023-04-25 08:50:05 +0300

    Admin puslapyje sukurti meniu punktus visiems sukurtiems modeliams

--- mysite/autoservisas/admin.py
+++ mysite/autoservisas/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import (VehicleModel,
+                     Vehicle,
+                     Service,
+                     Order,
+                     OrderLine)
 # Register your models here.
+admin.site.register(VehicleModel)
+admin.site.register(Vehicle)
+admin.site.register(Service)
+admin.site.register(Order)
+admin.site.register(OrderLine)
