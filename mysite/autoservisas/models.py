089bc6862a0f7a65fcfbe268dab3c1ec88f85d78
Gytis Vaicius <gyteniz@gmail.com>
2023-04-25 14:15:24 +0300

    Susikurti superuser vartotoją, prisijungti ir išbandyti įrašyti visų modelių įrašus

--- mysite/autoservisas/models.py
+++ mysite/autoservisas/models.py
@@ -5,11 +5,16 @@ class Service(models.Model):
     name = models.CharField(verbose_name="Pavadinimas", max_length=100)
     price = models.IntegerField(verbose_name="Kaina")
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class VehicleModel(models.Model):
     make = models.CharField(verbose_name="Gamintojas", max_length=50)
     model = models.CharField(verbose_name="Modelis", max_length=100)
 
+    def __str__(self):
+        return f"{self.make} {self.model}"
 
 class Vehicle(models.Model):
     plate = models.CharField(verbose_name="Valstybinis_Nr", max_length=6)
@@ -17,13 +22,22 @@ class Vehicle(models.Model):
     owner_name = models.CharField(verbose_name="Savininkas", max_length=50)
     vehicle_model = models.ForeignKey(to="VehicleModel",on_delete=models.SET_NULL, null=True)
 
+    def __str__(self):
+        return f"{self.vehicle_model} ({self.plate})"
+
 
 class Order(models.Model):
     date = models.DateTimeField(verbose_name="Data", auto_now_add=True)
     vehicle = models.ForeignKey(to="Vehicle", verbose_name="Automobilis", on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.vehicle} ({self.date})"
 
 
 class OrderLine(models.Model):
     order = models.ForeignKey(to="Order", on_delete=models.CASCADE)
     service = models.ForeignKey(to="Service", verbose_name="Paslauga", on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(verbose_name="Kiekis")
+
+    def __str__(self):
+        return f"{self.order.vehicle} {self.order.date}: {self.service}"
