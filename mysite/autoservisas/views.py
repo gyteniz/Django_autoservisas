55015778876f752f64e39b3c19c2eb271437c0cc
Gytis Vaicius <gyteniz@gmail.com>
2023-04-27 01:29:02 +0300

    3 pakeistas views.py, istraukiama statistika (taisytinas)

--- mysite/autoservisas/views.py
+++ mysite/autoservisas/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Service, VehicleModel, Vehicle, Order, OrderLine
+
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Labas, pasauli!")
+    num_services = Service.objects.all().count()
+    num_vehicle_models = VehicleModel.objects.all().count()
+    num_vehicles = Vehicle.objects.all().count()
+    num_orders = Order.objects.count()
+    num_order_lines = OrderLine.objects.all().count()
+
+    context = {
+        'num_services': num_services,
+        'num_vehicle_models': num_vehicle_models,
+        'num_vehicles': num_vehicles,
+        'num_orders': num_orders,
+        'num_order_lines': num_order_lines,
+    }
+    return render(request, 'index.html', context=context)
