f3d4f7f160e1eba8bf9e67de15bd24192d591db6
Gytis Vaicius <gyteniz@gmail.com>
2023-04-27 12:41:58 +0300

    3 Sukurti puslapį (ne admin), kuriame būtų matoma statistika: paslaugų kiekis, atliktų užsakymų kiekis, automobilių kiekis

--- mysite/autoservisas/views.py
+++ mysite/autoservisas/views.py
@@ -6,16 +6,12 @@ from .models import Service, VehicleModel, Vehicle, Order, OrderLine
 # Create your views here.
 def index(request):
     num_services = Service.objects.all().count()
-    num_vehicle_models = VehicleModel.objects.all().count()
-    num_vehicles = Vehicle.objects.all().count()
-    num_orders = Order.objects.count()
-    num_order_lines = OrderLine.objects.all().count()
+    num_vehicles = Vehicle.objects.count()
+    num_orders = Order.objects.filter(status__exact='i').count()
 
     context = {
         'num_services': num_services,
-        'num_vehicle_models': num_vehicle_models,
         'num_vehicles': num_vehicles,
         'num_orders': num_orders,
-        'num_order_lines': num_order_lines,
     }
     return render(request, 'index.html', context=context)
