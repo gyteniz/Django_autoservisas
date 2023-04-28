b33ec9c24376db6d92678660a1a5b57cd14ac9fb
Gytis Vaicius <gyteniz@gmail.com>
2023-04-28 17:37:49 +0300

    4 PAPILDOMAI: prideta meniu juosta

--- mysite/autoservisas/urls.py
+++ mysite/autoservisas/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('automobiliai/', views.automobiliai, name='automobilis'),
+    path('automobiliai/', views.automobiliai, name='automobiliai'),
     path('automobiliai/<int:automobilis_id>', views.automobilis, name='automobilis'),
     path('uzsakymai/', views.OrderListView.as_view(), name='uzsakymai'),
     path('uzsakymai/<int:pk>', views.OrderDetailView.as_view(), name='uzsakymas'),
