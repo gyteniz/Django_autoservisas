823f780c63416455edc7825a2e6ebd383acdc291
Gytis Vaicius <gyteniz@gmail.com>
2023-05-10 18:16:54 +0300

    9. Padaryti vartotojo registracijos formą pagal šioje pamokoje išmoktus žingsnius.

--- mysite/autoservisas/urls.py
+++ mysite/autoservisas/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('uzsakymai/<int:pk>', views.OrderDetailView.as_view(), name='uzsakymas'),
     path('search/', views.search, name='search'),
     path('my_orders/', views.MyOrderListView.as_view(), name='my_orders'),
+    path('register/', views.register, name='register'),
 ]
